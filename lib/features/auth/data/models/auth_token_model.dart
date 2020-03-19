a4476a63dac7002ab5c072acd7105609c2135998
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-03-19 15:51:03 +0530

    WIP

--- lib/features/auth/data/models/auth_token_model.dart
+++ lib/features/auth/data/models/auth_token_model.dart
@@ -1,6 +1,10 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
 import 'package:open_cloud_encryptor/common/errors/model_error.dart';
 
+part 'auth_token_model.g.dart';
+
+@JsonSerializable(nullable: false)
 class AuthTokenModel {
   final String token;
 
@@ -8,17 +12,10 @@ class AuthTokenModel {
     @required this.token,
   });
 
-  factory AuthTokenModel.fromJson(Map<String, dynamic> json) {
-    return AuthTokenModel(
-      token: json['token'] as String,
-    );
-  }
+  factory AuthTokenModel.fromJson(Map<String, dynamic> json) =>
+      _$AuthTokenModelFromJson(json);
 
-  Map<String, dynamic> toJson() {
-    return {
-      'token': token,
-    };
-  }
+  Map<String, dynamic> toJson() => _$AuthTokenModelToJson(this);
 }
 
 class AuthTokenModelError implements ModelError {
---
+++ lib/features/auth/data/models/auth_token_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_token_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthTokenModel _$AuthTokenModelFromJson(Map<String, dynamic> json) {
+  return AuthTokenModel(
+    token: json['token'] as String,
+  );
+}
+
+Map<String, dynamic> _$AuthTokenModelToJson(AuthTokenModel instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+    };
